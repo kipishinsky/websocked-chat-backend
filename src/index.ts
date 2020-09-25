68c5d9ac1f73ca37c39a2151d7a0a826ec9af59e
rbkmoneyukk@bk.ru <rbkmoneyukk@bk.ru>
2020-09-25 18:48:08 +0400

    add state messages

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,45 @@
 import express from 'express'
 import http from 'http'
-import socketio from 'socket.io'
+import io from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const socketBus = io(server)
+
+
+const stateMessages = [
+	{
+		userId: 'hdg2231', userName: 'Anton', userMessage: {
+			id: '6f3fyh', text: 'privet brodyga'
+		}
+	},
+	{
+		userId: 'hdg213211', userName: 'Igor', userMessage: {
+			id: '6f3fyh', text: 'privet anton'
+		}
+	},
+	{
+		userId: 'hdgddf211', userName: 'Sveta', userMessage: {
+			id: '6f32fsh', text: 'privet vsem!!!!!!'
+		}
+	}
+]
+
 
 app.get('/', (req, res) => {
 	res.send('server is currently running')
 })
 
-io.on('connection', (socket) => {
-	console.log('a user connected')
+socketBus.on('connection', (connection) => {
+
+	connection.on('client-message-sent', (message: string) => {
+		console.log(message)
+	})
+
+	connection.emit('init-messages-published', stateMessages)
+
+
+	console.log('USER CONNECTED')
 })
 
 const PORT = process.env.PORT || 3003
@@ -19,3 +47,4 @@ const PORT = process.env.PORT || 3003
 server.listen(PORT, () => {
 	console.log('listening on *:3003')
 })
+
